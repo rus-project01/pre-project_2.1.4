da7ad54807a9eeaaceaf626389499e56c02bce0f
rus <ruslanio.07@mail.ru>
2020-04-06 13:20:53 +0300

    .

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,13 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-
-    private final Deth8 deth;
-
     @Autowired
-    public Needle7(Deth8 deth) {
-        this.deth = deth;
-    }
+    private Deth8 deth;
 
     @Override
     public String toString() {
